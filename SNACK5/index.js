4ff093a3f34041b14cbf77dfeb69edabf3d38bf6
alan_omam <omamalan@hotmail.it>
2024-02-02 17:05:31 +0100

    FATTO SNACK5

--- SNACK5/index.js
+++ SNACK5/index.js
@@ -6,3 +6,15 @@ Stampa il cubo dei primi N numeri,
 dove N è un numero indicato dall’utente.
 */
 
+let numeroUtente = document.querySelector("#userNum")
+let cubo = document.querySelector("#cube")
+
+let numero = Number(prompt("inserisci un numero"))
+let numeri=[]
+
+for(let i=0 ; i<numero;i++){
+ numeri.push(Math.pow((i+1),3))
+}
+
+numeroUtente.innerHTML=numero
+cubo.innerHTML=numeri
