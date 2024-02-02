df698f7c69f835149bc1601d4b10ac63135f7af4
alan_omam <omamalan@hotmail.it>
2024-02-02 15:25:45 +0100

    SISTEMATO PARTE LOGICA DELLO SNACK1(i risultati sono visibili nel dom, tornerò a stilizzare meglio quando avrò fatto gli altri snacks)

--- SNACK1/index.js
+++ SNACK1/index.js
@@ -4,13 +4,22 @@ L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga.
 */
 
-let primaParola = "andy" 
-let secondaParola = "alan"
+let primaParola = prompt("inserisci la prima parola") 
+let secondaParola = prompt("inserisci la seconda parola") 
+
+let primoOutput = document.querySelector("#first")
+let secondoOutput = document.querySelector("#second")
+let risultato = document.querySelector("#result")
+
+primoOutput.innerHTML=primaParola
+secondoOutput.innerHTML=secondaParola
 
 if(primaParola.length > secondaParola.length){
-  console.log(primaParola)
+ risultato.innerHTML=primaParola
 }else if(primaParola.length < secondaParola.length){
-    console.log(secondaParola)
+    risultato.innerHTML=secondaParola
 }else{
-    console.log("sono lunghe uguali")
+    risultato.innerHTML= "sono lunghe uguali"
 }
+
+
