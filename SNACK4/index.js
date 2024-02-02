1facb2a41bb85ff2138ca5e4b88ecd351b4aa022
alan_omam <omamalan@hotmail.it>
2024-02-02 16:36:28 +0100

    FATTO SNACK4(aggiunto anche un test che verifica che il numero inserito abbia veramente 4 ciffre)

--- SNACK4/index.js
+++ SNACK4/index.js
@@ -4,3 +4,14 @@ Chiedi un numero di 4 cifre all’utente
 e calcola la somma di tutte le cifre che compongono il numero.
 */
 
+let sommaOutput = document.querySelector("#sum")
+let numero = prompt("dammi un numero a 4 ciffre")
+let somma = 0
+if(numero.length == 4){
+for(let i=0; i<numero.length; i++){
+ somma = somma+Number(numero.charAt(i))
+}
+sommaOutput.innerHTML=somma
+}else{
+    sommaOutput.innerHTML="inserisci un numero a 4 ciffre"
+}
