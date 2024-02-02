141ab46bc1b4ac9fee7b3efa35aae51e745c977a
alan_omam <omamalan@hotmail.it>
2024-02-02 17:26:43 +0100

    FATTO SNACK6

--- SNACK6/index.js
+++ SNACK6/index.js
@@ -3,3 +3,18 @@
  Snack 6:
 Calcola la somma e la media dei primi 10 numeri
  */
+let somma = document.querySelector("#add")
+let mediaNumeri = document.querySelector("#average")
+
+let sum = 0
+let numeri =[]
+
+for(let i= 0; i<10; i++ ){
+  numeri.push(i+1)  
+ sum = sum+numeri[i]    
+}
+let media = sum/10
+
+somma.innerHTML = sum
+mediaNumeri.innerHTML = media
+
