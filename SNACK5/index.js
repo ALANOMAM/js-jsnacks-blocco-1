d9d5a2c7dea944560beb6b3cf56976c5de7e5dfe
alan_omam <omamalan@hotmail.it>
2024-02-04 17:54:56 +0100

    FATTO SNACK5(aggiunto un test che verifica se ho inerito un numero, se non è il caso mi dice che il dato no  è un numero)

--- SNACK5/index.js
+++ SNACK5/index.js
@@ -8,12 +8,17 @@ dove N è un numero indicato dall’utente.
 
 let numeroUtente = document.querySelector("#userNum")
 let cubo = document.querySelector("#cube")
+let risposta =document.querySelector("#answer")
 
 let numero = Number(prompt("inserisci un numero"))
 let numeri=[]
 
+if(!isNaN(numero)){
 for(let i=0 ; i<numero;i++){
  numeri.push(Math.pow((i+1),3))
+}
+}else{
+    risposta.innerHTML = "Il dato inserito non è un numero"
 }
 
 numeroUtente.innerHTML=numero
